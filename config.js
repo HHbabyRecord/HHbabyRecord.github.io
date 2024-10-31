14579058b271e9dc198cbf5f9576c67ada0d654c
HHbabyRecord <1301935669@qq.com>
2024-10-31 22:53:59 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,52 @@
+var config = {
+    texts: [
+        "送给我",  
+        "心爱的姑娘", 
+        "今天是你的生日",
+        "这是我们在一起的",
+        "第一个生日！",
+        "今年来，我见过你",
+        "奇怪的样子",
+        "可爱的样子",
+        "嘟嘴的样子",
+        "都是我喜欢的样子",
+        "对了，礼物收到了吗？",
+        "今年要吃好好的",
+        "喂饱饱的",
+        "然后才有力气学习",
+        "和想我！",
+        "生日快乐~~",
+    ],
+
+    imgs: {
+        "送给我": "./imgs/songxinxin.jpeg",
+        "心爱的姑娘": "./imgs/10.jpg",
+        "今天是你的生日": "./imgs/shengri.jpeg",
+        "这是我们在一起的": "./imgs/zaiyiqi.png",
+        "第一个生日！": "./imgs/dangao.png",
+        "今年来，我见过你": "./imgs/1.jpg",
+        "奇怪的样子": "./imgs/14.jpg",
+        "可爱的样子": "./imgs/15.jpg",
+        "嘟嘴的样子": "./imgs/7.jpg",
+        "都是我喜欢的样子": "./imgs/woquandouyao.png",
+        "对了，礼物收到了吗？": "./imgs/chuo.gif",
+        "今年要吃好好的": "./imgs/chihaohehao.png",
+        "喂饱饱的": "./imgs/weini.png",
+        "然后才有力气学习": "./imgs/xuexi.png",
+        "和想我！": "./imgs/xiangwo.png",
+        "生日快乐~~": "./imgs/dangao.png"
+    },
+
+    desc: {
+        turn_on: "点我开始",
+        play: "上音乐",
+        bannar_coming: "搞点仪式",
+        balloons_flying: "好像还少点东西",
+        cake_fadein: "蛋糕？",
+        light_candle: "蜡烛？",
+        wish_message: "生日快乐～",
+        story: "A MESSAGE FOR YOU"
+    },
+
+    loveText: '爱你的陈先生~'
+};
